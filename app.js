d55d8943c0c4540199c13c5538857c27dfa2c5ed
Aquibe <60189864+aquibe@users.noreply.github.com>
2021-06-17 15:16:13 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,37 +2,51 @@ require('dotenv').config()
 
 const Discord = require('discord.js')
 const dude= new Discord.Client()
-const MongoClient = require('mongodb').MongoClient 
-const db_name=process.env.MONGO_DBNAME
-const mongoUri = "mongodb+srv://"+process.env.MONGO_USERNAME+":"+process.env.MONGO_PASSWORD+"@"+process.env.MONGO_CLUSTER+"/"+db_name
-const dbClient = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+//const MongoClient = require('mongodb').MongoClient 
+// const db_name=process.env.MONGO_DBNAME
+// const mongoUri = "mongodb+srv://"+process.env.MONGO_USERNAME+":"+process.env.MONGO_PASSWORD+"@"+process.env.MONGO_CLUSTER+"/"+db_name
+// const dbClient = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
 
-dbClient.connect((err)=>{
-    if(err){
-        console.log(err)
-    }else{
-       console.log('db connected')
+// dbClient.connect((err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//        console.log('db connected')
+//     }
+// })
+
+dude.on('guildMemberAdd',async(guildMember)=>{
+    const role = 4
+    if(role==0){
+        guildMember.send('type register to complete registration')
+    }else if(role==1){
+        guildMember.roles.add(guildMember.guild.roles.cache.find(role => role.name == "member"))
+    }else if(role==2){
+        guildMember.roles.add(guildMember.guild.roles.cache.find(role => role.name == "member"))
+        guildMember.roles.add(guildMember.guild.roles.cache.find(role => role.name == "girls-power"))
+    }else if(role==3){
+        guildMember.roles.add(guildMember.guild.roles.cache.find(role => role.name == "member"))
+        guildMember.roles.add(guildMember.guild.roles.cache.find(role => role.name == "siasian"))
+    }else if(role==4){
+        guildMember.roles.add(guildMember.guild.roles.cache.find(role => role.name == "member"))
+        guildMember.roles.add(guildMember.guild.roles.cache.find(role => role.name == "siasian"))
+        guildMember.roles.add(guildMember.guild.roles.cache.find(role => role.name == "girls-power"))
     }
 })
 
-dude.on('guildMemberAdd',async()=>{
-           
-/*
-    if bot :
-        inform admins
-    else :
-        if checkUser:
-            oldUser(userId)
-        else:
-          newUser(userId)
-*/
+dude.on('guildMemberRemove',async(guildMember)=>{
+    console.log('left')
 })
 
 dude.on('message',async(message)=>{
+    console.log('message')
+    const userId=message.author.id;
+    const commands = message.content.split(' ')
     if(!message.author.bot){
         if(message.channel.type=='dm'){
-            const userId=message.author.id;
-            //answer questions
+            if(commands[0].toLowerCase()=='register'){
+                addUser(userId,message.content.substring(9))
+            }
             messageAdmin(userId,message.content)
         }else if(message.channel.type=='text'){
             //answer questions
@@ -43,6 +57,14 @@ dude.on('message',async(message)=>{
     answer questions
 */
 })
+
+function addUser(userId,name){
+    if(checkUser(userId)){
+
+    }else{
+       // add to db
+    }
+}
 
 dude.on('ready',()=>{
     console.log('logged in as '+dude.user.tag)
@@ -65,7 +87,8 @@ function oldUser(userId){
 */ 
 }
 
-function newUser(UserId){
+async function newUser(userId){
+ 
 /* 
     get name
     get college
